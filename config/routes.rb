726506e98cfd32daeafe5228be61167c531d69f6
igor <fedyaninigor@gmail.com>
2018-06-05 10:05:10 +0300

    add name column users table RegistrationsController

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
 
+  devise_for :users, :controllers => {:registrations => "registrations"}
+
   devise_scope :user do
   get 'login', to: 'devise/sessions#new'
 end
